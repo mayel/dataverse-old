042f155d821c472935d514047edb8351b7ba2554
Mayel de Borniol <mayel@deborniol.com>
2018-01-05 19:01:42 +0200

    token

--- src/ctrl/payments_stripe.php
+++ src/ctrl/payments_stripe.php
@@ -20,25 +20,26 @@ class DataverseStripe extends AdamPaterson\OAuth2\Client\Provider\Stripe {
 
 	public function getUserData($access_token){
 
-		$msg .= sprintf('Using access_token %s --- ', $access_token);
+		print_r($access_token);
+		
+		$msg .= '<br>Access Token: ' . $access_token->getToken() . "<br>";
+    $msg .= 'Refresh Token: ' . $access_token->getRefreshToken() . "<br>";
+    $msg .= 'Expired in: ' . $access_token->getExpires() . "<br>";
+    $msg .= 'Already expired? ' . ($access_token->hasExpired() ? 'expired' : 'not expired') . "<br>";
 
 	// With an access token, let's now get the user's details
-		$account = $provider->getResourceOwner($access_token);
+		$account = $this->getResourceOwner($access_token);
 
 		// Use these details to match the user, or create a new profile
-		$msg .= sprintf('Welcome %s! ', $account->getDisplayName());
+		$msg .= sprintf('<br>Welcome %s! ', $account->getDisplayName());
 
-		$msg .= sprintf('Your business name: %s ', $account->getBusinessName());
-		$msg .= sprintf('Your currency: %s ', $account->getDefaultCurrency());
+		$msg .= sprintf('<br>Your business name: %s ', $account->getBusinessName());
+		$msg .= sprintf('<br>Your currency: %s ', $account->getDefaultCurrency());
 		// $msg .= sprintf('Your currencies: %s ', $account->getCurrenciesSupported());
-		$msg .= sprintf('Your country: %s ', $account->getCountry());
-		$msg .= sprintf('Your statement descriptor: %s ', $account->getStatementDescriptor());
-		$msg .= sprintf('Your email: %s ', $account->getEmail());
+		$msg .= sprintf('<br>Your country: %s ', $account->getCountry());
+		$msg .= sprintf('<br>Your statement descriptor: %s ', $account->getStatementDescriptor());
+		$msg .= sprintf('<br>Your email: %s ', $account->getEmail());
 
-		// Use this to interact with an API on the users behalf
-		$user_token = $access_token->getToken();
-
-		$msg .= sprintf('Ready to use user_token %s --- ', $user_token);
 
 		return $msg;
 
@@ -52,9 +53,9 @@ $app->get('/payments/setup/stripe', function () use ($app) {
 
 	  $provider = new \DataverseStripe();
 
-	  if (isset($_GET['access_token'])) { // Look up a user's data with a previously know access_token
+	  if (isset($_GET['access_token'])) { // can look up a user's data with a previously know access_token
 
-			$msg = $provider->getUserData($_GET['access_token']);
+			$access_token = $_GET['access_token'];
 
 		} elseif (!isset($_GET['code'])) { // If we don't have an access_token or authorization code then redirect the user to get one
 
@@ -63,23 +64,24 @@ $app->get('/payments/setup/stripe', function () use ($app) {
 	    header('Location: '.$authUrl);
 	    exit;
 
-	  // Check given state against previously stored one to mitigate CSRF attack
-	  } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-
-	      unset($_SESSION['oauth2state']);
-	      return ('Invalid state. ');
-
-	  } else {
+		} elseif (!empty($_GET['state']) && ($_GET['state'] == $_SESSION['oauth2state'])) { // Check given state against previously stored one to mitigate CSRF attack
 
 			// Try to get an access token (using the authorization code grant)
 	    $access_token = $provider->getAccessToken('authorization_code', [
 	      'code' => $_GET['code']
 	    ]);
 
-			// Optional: Now you have a token you can look up a users profile data
-			if($access_token) $msg = $provider->getUserData($access_token);
+		} else {
+
+			$msg .= ('Invalid state. ');
+			unset($_SESSION['oauth2state']);
 
     }
+
+
+		// Optional: if we have a token, look up a user's profile data
+		if($access_token) $msg .= $provider->getUserData($access_token);
+
 
 	} catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
 
