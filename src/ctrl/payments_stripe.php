ff858e8444e080e995259b1c041b95f5df805fb2
Mayel de Borniol <mayel@deborniol.com>
2018-01-05 17:57:11 +0200

    error handling

--- src/ctrl/payments_stripe.php
+++ src/ctrl/payments_stripe.php
@@ -10,15 +10,14 @@ $app->get('/payments/setup/stripe', function () use ($app) {
     'redirectUri'       => $bv->config->home_url.'/payments/setup/stripe',
 	]);
 
-  if (!isset($_GET['code'])) {
+  if (!isset($_GET['code'])) { // If we don't have an authorization code then get one
 
-    // If we don't have an authorization code then get one
     $authUrl = $provider->getAuthorizationUrl();
     $_SESSION['oauth2state'] = $provider->getState();
     header('Location: '.$authUrl);
     exit;
 
-    // Check given state against previously stored one to mitigate CSRF attack
+  // Check given state against previously stored one to mitigate CSRF attack
   } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
 
       unset($_SESSION['oauth2state']);
@@ -26,30 +25,36 @@ $app->get('/payments/setup/stripe', function () use ($app) {
 
   } else {
 
-    // Try to get an access token (using the authorization code grant)
-    $token = $provider->getAccessToken('authorization_code', [
-      'code' => $_GET['code']
-    ]);
-
-    // Optional: Now you have a token you can look up a users profile data
     try {
 
+			// Try to get an access token (using the authorization code grant)
+	    $access_token = $provider->getAccessToken('authorization_code', [
+	      'code' => $_GET['code']
+	    ]);
+
+
+			// Optional: Now you have a token you can look up a users profile data
+
       // We got an access token, let's now get the user's details
-      $account = $provider->getResourceOwner($token);
+      $account = $provider->getResourceOwner($access_token);
 
       // Use these details to create a new profile
-      return sprintf('Welcome %s!', $account->getDisplayName());
+      $msg .= sprintf('Welcome %s! ', $account->getDisplayName());
+
+
+			// Use this to interact with an API on the users behalf
+			$user_token = $access_token->getToken();
+
+			$msg .= sprintf('Ready to use token %s! ', $user_token);
 
     } catch (Exception $e) {
 
       // Failed to get user details
-      return ('Oh dear...');
+			print_r($e);
+      $msg .= ('Oh dear... An error occured. ');
     }
 
-    // Use this to interact with an API on the users behalf
-    $token = $token->getToken();
-
-    return sprintf('Ready with token %s!', $token);
+		return $msg;
   }
 })
 ;
